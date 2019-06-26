63854aed394e6e30569e174e8361974ae5b2faf1
Harish S <harish@everest.engineering>
2019-06-26 19:08:56 +0530

    Add Landing page

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react';
+import './Footer.scss';
+
+class Footer extends PureComponent {
+  state = {};
+  render() {
+    return <div>Footer</div>;
+  }
+}
+
+export default Footer;
---
+++ src/Landing.js
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react';
+import './Landing.scss';
+
+class Landing extends PureComponent {
+  render() {
+    return (
+      <div className='intro'>
+        <h1>S Harish</h1>
+      </div>
+    );
+  }
+}
+
+export default Landing;
